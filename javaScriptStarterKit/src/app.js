69f671228047983b223717df782b0e526d40f592
vugar84 <aliyev@live.ru>
2021-10-20 12:43:19 +0400

    10.20.2021

---
+++ javaScriptStarterKit/src/app.js
@@ -0,0 +1,9 @@
+console.log("Merhaba kodlama.io")
+
+let konutKredileri = ["Konut Kredisi", "Emlak Kredileri", "Kamu Konut kredisi"]
+console.log("<ul>")
+for(let i=0; i<konutKredileri.length; i++){
+    console.log("<li>"+konutKredileri[i]+"<li>")
+}
+
+console.log("<ul>")
